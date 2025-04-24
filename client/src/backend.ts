fad317323116face12ff60c2ca64a9eb9447ffce
Max Gomes <maxgomes92@gmail.com>
2025-04-25 01:56:34 +0200

    endpoint

--- client/src/backend.ts
+++ client/src/backend.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = process.env.NODE_ENV === "development" ? "ws://localhost:2567" : "ws://monoscopic-race-148203050277.europe-north2.run.app";
+export const BACKEND_URL = "ws://0.tcp.eu.ngrok.io:19265"
 
 export const BACKEND_HTTP_URL = BACKEND_URL.replace("ws", process.env.NODE_ENV === "development" ? "http" : "https");
