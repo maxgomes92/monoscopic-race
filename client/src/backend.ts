2cd5b3baf73d78e9b2be9cae408e4e9c28af60ba
Max Gomes <maxgomes92@gmail.com>
2025-04-25 00:57:33 +0200

    env var

--- client/src/backend.ts
+++ client/src/backend.ts
@@ -1,7 +1,3 @@
-// export const BACKEND_URL = (window.location.href.indexOf("localhost") === -1)
-//     ? `${window.location.protocol.replace("http", "ws")}//${window.location.hostname}${(window.location.port && `:${window.location.port}`)}`
-//     : "ws://localhost:2567"
-
-export const BACKEND_URL = process.env.BACKEND_HOST;
+export const BACKEND_URL = process.env.NODE_ENV === "development" ? "ws://localhost:2567" : "ws://monoscopic-race-148203050277.europe-north2.run.app";
 
 export const BACKEND_HTTP_URL = BACKEND_URL.replace("ws", "http");
